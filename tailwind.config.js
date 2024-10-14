54793dd23206322dea719b21a7f4d165dd935d18
efobi kingsley <efobikings@gmail.com>
2024-10-14 16:10:09 +0100

    yes commited

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screen:{
+      sm: '480px',
+      md: '76'
+    }
+    extend: {},
+  },
+  plugins: [],
+}
+
